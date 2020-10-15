941f909639841edd2c0025789c38b485e78e921b
Captain-Vorobey <vorobyov_egor@bk.ru>
2020-10-15 16:33:58 +0300

    Found the free time intervals

--- spec/intervals_spec.rb
+++ spec/intervals_spec.rb
@@ -4,8 +4,16 @@ require 'rspec/autorun'
 require_relative '../lib/script.rb'
 
 describe Intervals do
-  let(:lies_and_push) { Intervals.new }
-  it 'finds the intervals of times array' do
-    expect(Intervals.new.lies_and_push([['10:00', '10:20'], ['10:40', '11:00'], ['10:50', '12:00'], ['12:00', '13:00'], ['10:00', '10:20']])).to eq([['10:00', '10:20'], ['10:40', '13:00']])
+  let(:free_time) { Intervals.new }
+  it 'should return the free parts of our time intervals' do
+    arr = [['10:00', '10:20'], ['10:40', '11:00'], ['10:50', '12:00'], ['12:00', '13:00'], ['10:00', '10:20']]
+    res = [['10:20', '10:40']]
+    expect(Intervals.free_time(arr)).to eq(res)
+  end
+
+  it 'should return the free parts of our time intervals' do
+    arr = [['10:00', '10:20'], ['10:00', '10:20'], ['10:40', '11:00'], ['10:30', '10:35'], ['10:50', '12:00'], ['12:00', '13:00'], ['14:40', '15:00'], ['14:50', '16:00']]
+    res = [["10:20", "10:30"], ["10:35", "10:40"], ["13:00", "14:40"]]
+    expect(Intervals.free_time(arr)).to eq(res)
   end
 end
